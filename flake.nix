5ecaabb62638ead189784bdada94b1cd9f72612c
Lachlan Wilger <github.defender025@passmail.net>
2025-02-22 19:35:46 -0800

    Initial Commit

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  description = "Home Manager configuration of lwilger";
+
+  inputs = {
+    # Specify the source of Home Manager and Nixpkgs.
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    stylix.url = "github:danth/stylix";
+    nixgl = {
+      url = "github:nix-community/nixGL";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixvim = {
+      url = "github:nix-community/nixvim";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
+    };
+    alejandra = {
+      url = "github:kamadorueda/alejandra/3.1.0";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    nixpkgs,
+    home-manager,
+    stylix,
+    nixgl,
+    nixvim,
+    alejandra,
+    ...
+  }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
+    };
+  in {
+    homeConfigurations."lwilger" = home-manager.lib.homeManagerConfiguration {
+      inherit pkgs;
+
+      extraSpecialArgs = {
+        inherit nixgl nixvim;
+      };
+      modules = [stylix.homeManagerModules.stylix ./home.nix];
+    };
+    formatter.${system} = alejandra.defaultPackage.${system};
+  };
+}
---
+++ home.nix
@@ -0,0 +1,125 @@
+{
+  config,
+  pkgs,
+  lib,
+  nixgl,
+  nixvim,
+  ...
+}: {
+  home.username = "lwilger";
+  home.homeDirectory = "/home/lwilger";
+
+  home.stateVersion = "24.11";
+
+  targets.genericLinux.enable = true;
+
+  imports = [nixvim.homeManagerModules.nixvim];
+
+  nixGL.packages = nixgl.packages;
+  nixGL.defaultWrapper = "mesa";
+  nixGL.installScripts = ["mesa"];
+
+  programs.home-manager.enable = true;
+
+  programs.zsh = {
+    enable = true;
+    shellAliases = {
+      z = "zellij";
+      v = "nvim";
+      t = "tmux";
+      hm = "home-manager";
+
+      # Git Aliases
+      g = "git";
+      ga = "git add";
+      gaa = "git add -A";
+      gc = "git commit";
+      gcl = "git clone";
+      gp = "git push";
+
+      # nix-shell use zsh
+      nix-shell = "nix-shell --run ${pkgs.zsh}/bin/zsh";
+    };
+    # run zsh in a nix develop environment
+    initExtra = ''
+      nix() {
+        if [[ $1 == "develop" ]]; then
+          shift
+          command nix develop -c ${pkgs.zsh}/bin/zsh "$@"
+        else
+          command nix "$@"
+        fi
+      }
+    '';
+    sessionVariables.EDITOR = "nvim";
+  };
+
+  stylix = {
+    enable = true;
+    polarity = "dark";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/dracula.yaml";
+    image = ./mountain-background.jpg;
+    fonts = {
+      monospace = {
+        package = pkgs.nerd-fonts.jetbrains-mono;
+        name = "JetBrainsMono Nerd Font Mono";
+      };
+      serif = {
+        package = pkgs.carlito;
+        name = "Carlito";
+      };
+      sansSerif = {
+        package = pkgs.poly;
+        name = "Poly";
+      };
+    };
+  };
+
+  qt.enable = true;
+
+  programs.btop.enable = true;
+  programs.bat.enable = true;
+
+  programs.kitty = {
+    enable = true;
+    package = config.lib.nixGL.wrap pkgs.kitty;
+    shellIntegration.enableBashIntegration = true;
+    shellIntegration.enableZshIntegration = true;
+    settings.shell = "zsh";
+    font.size = lib.mkForce 11;
+  };
+
+  programs.nixvim = import ./nixvim.nix;
+
+  programs.starship = {
+    enable = true;
+    enableBashIntegration = true;
+    enableZshIntegration = true;
+    settings = {
+      package.disabled = true;
+      elixir.disabled = true;
+    };
+  };
+
+  programs.zellij = {
+    enable = true;
+    enableBashIntegration = false;
+    enableZshIntegration = false;
+    settings = {
+      pane_frames = false;
+      default_layout = "compact";
+    };
+  };
+
+  programs.git = {
+    enable = true;
+    userEmail = "github.defender025@passmail.net";
+    userName = "Lachlan Wilger";
+    signing = {
+      key = "2EE29D3CE347115D";
+      signByDefault = true;
+    };
+    ignores = ["*~" "*.swp"];
+    extraConfig.init.defaultBranch = "main";
+  };
+}
