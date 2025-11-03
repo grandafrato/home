f5a1c71cb5aeeba1ae74d2b9948534ba42acd887
Lachlan Wilger <github.defender025@passmail.net>
2025-11-03 13:55:53 -0800

    replace nix with lix

--- flake.nix
+++ flake.nix
@@ -21,6 +21,15 @@
       url = "github:winapps-org/winapps";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    lix = {
+      url = "https://git.lix.systems/lix-project/lix/archive/main.tar.gz";
+      flake = false;
+    };
+    lix-module = {
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/main.tar.gz";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.lix.follows = "lix";
+    };
   };
 
   outputs = {
@@ -30,6 +39,8 @@
     nixvim,
     nixos-hardware,
     winapps,
+    lix,
+    lix-module,
     ...
   } @ inputs: let
     system = "x86_64-linux";
@@ -55,6 +66,7 @@
             extraSpecialArgs = {inherit nixvim;};
           };
         }
+        lix-module.nixosModules.default
       ];
     };
     formatter.${system} = pkgs.alejandra;
